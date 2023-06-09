8c505de9a0a734c2cab9743290658f60517426f2
Bilal Maqsood <bilalsweet786@gmail.com>
2023-06-09 11:20:53 +0500

    Version Assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').vue()
     .sass('resources/sass/app.scss', 'public/css').sourceMaps();
+
+if (mix.inProduction()) {
+    mix.version();
+}
